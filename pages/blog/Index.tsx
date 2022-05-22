7becf8dd7bf6c4718c6f4d87da84326414ca1f3e
AdrielAO <adrielarochaoronoz@gmail.com>
2022-05-22 20:54:18 +0100

    💄 improving the blog and personal info

--- pages/blog/Index.tsx
+++ pages/blog/Index.tsx
@@ -1,11 +1,17 @@
-import { Col, Container, Row, Text } from "@nextui-org/react"
-import { getAllPostId, getAllpostIDs } from "../../utils/posts"
+import { Col, Container, Link, Row, Text } from "@nextui-org/react"
+import { getSortedPostsData } from "../../utils/posts"
 import { useRouter } from "next/router"
-import Link from "next/link"
 import style from "./blog.module.css"
+import NavBar from "../../components/navBar/NavBar"
 
 interface Props {
-	posts: string[]
+	posts: post[]
+}
+
+interface post {
+	id: string
+	title: string
+	date: string
 }
 
 const Index = ({ posts }: Props) => {
@@ -16,40 +22,54 @@ const Index = ({ posts }: Props) => {
 	const handleRouter = (url: string) => route.push(`/posts/${url}`)
 
 	return (
-		<Container display='flex' css={{ backgroundColor: "$tertiary" }}>
-			<Container xs display='flex' css={{ backgroundColor: "$secondary" }}>
-				<Row>
-					<Col>
-						<Text h1>El blog Oronoz</Text>
-						<Text>Para todo hay una primera vez, hasta para un blog. </Text>
-					</Col>
-				</Row>
+		<>
+			<NavBar />
+			<Container className={style.container} display='flex'>
+				<Container xs display='flex'>
+					<Row>
+						<Col>
+							<Text h1>El blog Oronoz</Text>
+							<Text size={25}>
+								Para todo hay una primera vez, hasta para un blog.{" "}
+							</Text>
+						</Col>
+					</Row>
 
-				<Row justify='space-around'>
-					<Text size={30}>👇👇</Text>
-					<Text size={30}>👀</Text>
-					<Text size={30}>👇👇</Text>
-				</Row>
+					<Row justify='space-around'>
+						<Text size={30}>👇👇</Text>
+						<Text size={30}>👀</Text>
+						<Text size={30}>👇👇</Text>
+					</Row>
 
-				{posts.map((post, index) => {
-					return (
-						<Row
-							justify='center'
-							className={index % 2 === 0 ? style.odd : style.even}
-							key={post}>
-							<Link href={`/posts/${post}`}>{post}</Link>
-						</Row>
-					)
-				})}
+					{posts.map((post, index) => {
+						return (
+							<Container
+								display='flex'
+								direction='column'
+								justify='space-between'
+								className={index % 2 === 0 ? style.odd : style.even}
+								key={post.id}>
+								<Row>
+									<Link css={{ margin: "$3" }} href={`/posts/${post.id}`}>
+										{post.title}
+									</Link>
+								</Row>
+								<Row css={{ margin: "$2" }}>
+									<Text>{post.date}</Text>
+								</Row>
+							</Container>
+						)
+					})}
+				</Container>
 			</Container>
-		</Container>
+		</>
 	)
 }
 
 export default Index
 
 export async function getStaticProps() {
-	const posts = getAllpostIDs()
+	const posts = getSortedPostsData()
 
 	return {
 		props: {
